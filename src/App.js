986a8f681f97e95ed83048328a9579edb6c8f952
charcodes24 <charcodes24@gmail.com>
2021-08-18 13:53:20 -0700

    Initial GET fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import NavBar from './NavBar';
@@ -6,12 +7,31 @@ import QuizHome from './QuizComponents/QuizHome';
 import FunFactsContainer from './FunFactsComponents/FunFactsContainer';
 
 function App() {
+  const [states, setStates] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3004/states")
+      .then((res) => res.json())
+      .then((states) => {
+        const shuffledArray = states.sort((a, b) => 0.5 - Math.random());
+        setStates(shuffledArray);
+      });
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
       <Switch>
-        <Route path="/funfacts" component={FunFactsContainer} />
-        <Route path="/" component={QuizHome} />
+        <Route path="/funfacts">
+          <FunFactsContainer
+            states={states}
+          />
+        </Route>
+        <Route path="/">
+          <QuizHome
+            states={states}
+          />
+        </Route>
       </Switch>
     </div>
   );
